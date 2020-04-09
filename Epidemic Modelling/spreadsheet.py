f5f5bd0ca8ffaa1f3b5731e186623e34e054c0c5
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-04-10 01:30:41 +0700

    remove pandas

--- Epidemic Modelling/spreadsheet.py
+++ Epidemic Modelling/spreadsheet.py
@@ -1,7 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
-import pandas as pd
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name('C:/Users/ibpra/Documents/API/client_secret.json', scope)
