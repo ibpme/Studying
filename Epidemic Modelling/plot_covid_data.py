a3cb6ac123aca8a38b1172c938fbca820ae60640
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-04-16 23:35:02 +0700

    <html update>

--- Epidemic Modelling/plot_covid_data.py
+++ Epidemic Modelling/plot_covid_data.py
@@ -2,7 +2,14 @@ from spreadsheet import data,time
 import matplotlib.pyplot as plt
 import numpy as np
 # dir(plt)
-dates_int = np.linspace(start = 0, stop = len(time) , num=len(time))
-plt.plot(dates_int, data[1],scalex=True)
-plt.xticks(ticks=dates_int, labels=time , rotation=90)
+# dates_int = np.linspace(start = 0, stop = len(time) , num=len(time))
+# plt.plot(dates_int, data[1],scalex=False,linestyle='-')
+# plt.xticks(ticks=dates_int, labels=time , rotation=90)
+# plt.show()
+
+plt.scatter(data[1],data[5])
+plt.ylabel('New Cases each Day')
+plt.xlabel('Number of Total Cases')
+# plt.xscale('log')
+# plt.yscale('log')
 plt.show()
